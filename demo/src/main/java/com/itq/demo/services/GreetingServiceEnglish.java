46adef13fe41683dd2b45f511fe8b5f752528c15
Eduardo <eduardohdzc@gmail.com>
2017-08-30 20:00:40 -0500

    Add property to define greeting message and change properties file to be a yml

--- demo/src/main/java/com/itq/demo/services/GreetingServiceEnglish.java
+++ demo/src/main/java/com/itq/demo/services/GreetingServiceEnglish.java
@@ -1,13 +1,18 @@
 package com.itq.demo.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Profile("en")
-public class GreetingServiceEnglish implements GreetingService{
+public class GreetingServiceEnglish implements GreetingService {
+
+    @Value("${greeting}")
+    private String greeting;
+
     @Override
     public String sayHello() {
-        return "Hello World!";
+        return greeting;
     }
 }
--- demo/src/main/java/com/itq/demo/services/GreetingServiceSpanish.java
+++ demo/src/main/java/com/itq/demo/services/GreetingServiceSpanish.java
@@ -1,5 +1,6 @@
 package com.itq.demo.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -7,8 +8,11 @@ import org.springframework.stereotype.Service;
 @Profile("es")
 public class GreetingServiceSpanish implements GreetingService
 {
+    @Value("${greeting}")
+    private String greeting;
+
     @Override
     public String sayHello() {
-        return "Hola Mundo!";
+        return greeting;
     }
 }
