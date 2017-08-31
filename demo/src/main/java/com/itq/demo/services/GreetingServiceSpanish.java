c0ea9d7d170fb37c8c56457910a94ad96de5d01c
Eduardo <eduardohdzc@gmail.com>
2017-08-30 19:41:14 -0500

    Define profile for greeting implementations

--- demo/src/main/java/com/itq/demo/services/GreetingServiceSpanish.java
+++ demo/src/main/java/com/itq/demo/services/GreetingServiceSpanish.java
@@ -1,8 +1,10 @@
 package com.itq.demo.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("es")
 public class GreetingServiceSpanish implements GreetingService
 {
     @Override
